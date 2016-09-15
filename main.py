3c2b8a7c772f5b2e697094a760dd23225c0c869a
grantdeford <grant.deford@gmail.com>
2016-09-15 09:29:10 -0500

    before break

--- main.py
+++ main.py
@@ -15,11 +15,81 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname('_file_'), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class Post(db.Model):
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+#Text property is over 500 char
+
+#class Initial(Handler):
+#    def get(self):
+#        self.render('base.html')
+
+#    def post(self):
+#        subject = self.request.get('subject')
+#        content = self.request.get('content')
+#        self.write(subject, content)
+
+
+class MainBlog(Handler):
+#    def list_post(self, subject="", content="", created=""):with self.render+get
     def get(self):
-        self.response.write('Hello world!')
+        results = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC LIMIT 5")
+        results_order = {"results" : results}
+        self.render('mainblog.html', **results_order)
+#        self.render('mainblog.html', subject=subject, content=content,
+#                    created=created, results=results)
+
+#    def get(self):
+#        self.render list_post()
+        #not using base.html
+
+class NewPost(Handler):
+    def get(self):
+        self.render('newpost.html')
+    def post(self):
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+        created = self.request.get('created')
+
+        if subject and content: #and created:
+            user_post = Post(subject=subject, content=content)#, created=created)
+            #do you have to add date?calling on Post db model
+            user_post.put()
+            self.redirect('/')
+        else:
+            error = "You need a subject and content." # and a date."
+            self.render('newpost.html', subject=subject, content=content, error=error)
+#           just self.render('newpost.html')??????
+
+#class ViewPostHandler(webapp2.RequestHandler):
+    #def get(self, id):
+        #self.response.write(% id)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+#    ('/', Initial),
+#    ('/mainblog', MainBlog),
+    ('/', MainBlog),
+    ('/newpost', NewPost)
+#    webapp2.Route('/?/<id:\d+>', ViewPostHandler)
 ], debug=True)
